5e5ab1a95f7388995783e5eb2c54b63a955c5034
Jean Val Jaan <jeanvaljaan@gmail.com>
2021-04-05 00:01:18 +0300

    added a counter, rearrange num interval to 0-1000

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 button = btn.addEventListener('click', isEqual);
-let rndm = parseInt(100*Math.random());
+let rndm = parseInt(1000*Math.random());
 let er = document.getElementById("err");
 let timer;
-function isEqual () {
-    let num = document.getElementById("num").value;
-    console.log(num);
-    console.log(rndm);
-    console.log(er);
-    if (num == rndm){
-        er.textContent = "Congrats you guessed right! After 3 seconds i will pick a new number";
-        timer = setTimeout(reload, 3000);
-        
-    }else if (num < rndm){
-        er.textContent = "A little bit higher";
+counter = 10;
+    function isEqual () {
+        let num = document.getElementById("num").value;
+        console.log(num);
+        console.log(rndm);
+        console.log(er);
+        if (counter == 0){
+            er.textContent = "You lost the game! Dont worry after 3 seconds i will pick a new number.";
+            timer = setTimeout(reload, 3000);
+
+        } else if (num == rndm){
+            er.textContent = "Congrats you guessed right! After 3 seconds i will pick a new number";
+            timer = setTimeout(reload, 3000);
+            
+        }else if (num < rndm){
+            er.textContent = `A little bit higher. You have ${counter} chances left`;
+            counter--
+        }
+        else {
+            er.textContent = `A little bit lower. You have ${counter} chances left`;
+            counter--
+        }
+     num = null;   
     }
-    else {
-        er.textContent = "A little bit lower";
+    function reload(){
+        location.reload();
     }
- num = null;   
-}
-function reload(){
-    location.reload();
-}
+ 
